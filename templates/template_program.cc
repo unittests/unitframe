c73393d043259a68f762dc0ef1709ad885113188
unknown <snsokolov@users.noreply.github.com>
2015-12-13 14:26:00 -0800

    Minor change

--- templates/template_program.cc
+++ templates/template_program.cc
@@ -21,7 +21,7 @@ class __Class__ { public:
 ///////////////////////////////////////////////////////////////////////////////
 
 
-// Cppunit - C++ Unit testing TDD framework (github.com/cppunit/cppunit)
+// CPlusPlusUnit - C++ Unit testing TDD framework (github.com/cppunit/cppunit)
 class Cppunit { public:
     #define CHECK(a,b)  check<long long>(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__);
     #define CHECKT(a)   check<bool>(a, true, #a, "true", __FILE__, __LINE__, __FUNCTION__);
